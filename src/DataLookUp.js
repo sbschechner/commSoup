eb987b9d48302806d0fe0bf51e10f611ed05bf50
benjy <benjamin.schechner@gmail.com>
2017-08-09 22:01:28 -0400

    creating sorting method

--- src/DataLookUp.js
+++ src/DataLookUp.js
@@ -67,6 +67,7 @@ findDistance(userLat, userLong, dataLat, dataLong){
             dist = dist * 180/Math.PI //converting back to degrees
             dist = dist * 60 * 1.1515 // converting back to normal miles (not nautical miles)
             return dist
+
     }
 
 accessData(){ //THIS SHOULD FIND THE TOP 5 CLOSEST
@@ -88,127 +89,44 @@ fetch("https://evening-thicket-30478.herokuapp.com/soupKitchens", {
      .then(data => {
         console.log("hello - we have hit endpoint for the DB");
         console.log(data);
-      })
-      .catch(error =>{
-      console.error(error)
-      })
 
       var orderedPlaces = [];
       var dataLat = data.kitchens[0].lat;
       var dataLong = data.kitchens[0].long;
-      var firstDistance = findDistance(this.props.userLat, this.props.userLong, dataLat,dataLong)
+      var firstDistance = this.findDistance(this.props.userLat, this.props.userLong, dataLat,dataLong)
       var placeObj = {
         id : data.kitchens[0].id,
         dist : firstDistance,
       }
       orderedPlaces.push(placeObj);
 
-      for(i=1;i<Object.values(data).length;i++){
+      for(var i=1;i<data.kitchens.length;i++){
 
          var currentLat = data.kitchens[i].lat;
       var currentLong = data.kitchens[i].long;
-      var currentDistance = findDistance(this.props.userLat, this.props.userLong, currentLat,currentLong)
+      var currentDistance = this.findDistance(this.props.userLat, this.props.userLong, currentLat,currentLong)
       var currentObj = {
         id : data.kitchens[i].id,
         dist : currentDistance,
       }
+
       orderedPlaces.push(currentObj);
     }
 
     console.log(orderedPlaces)
-}
+            }) //only have access to data between here and fetch
 
 
-/*
-      for(i=1; i<Object.values(data).length; i++){
-        var minArr = clos;
-        var min2Arr = closestArr[1];
-        dataLat = data[i].FIELD11; //need to make sure this is the correct path but cant see the data?
-        dataLong = data[i].FIELD12;
-          function distance(userLat, userLong, dataLat, dataLong) {
-            var radlat1 = Math.PI * userLat/180
-            var radlat2 = Math.PI * dataLat/180
-            var theta = userLong-dataLong
-            var radtheta = Math.PI * theta/180
-            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            dist = Math.acos(dist)
-            dist = dist * 180/Math.PI //converting back to degrees
-            dist = dist * 60 * 1.1515 // converting back to normal miles (not nautical miles)
-            if(dist < minArr){
-              if(dist <min2Arr){
-                console.log ("it is the smallest Distance");
-                closestArr[1] = dist;
-                idArr[1] = data[i]._id; // make sure this takes the ID of the object 
-              }
-             else{
-              console.log("it is the 2nd smallest distance");
-              closestArr[0] = dist;
-              idArr[0] = data[i]._id;
-              }
-          }
-      } 
-    } //end of the for loop
-      
-console.log("we have found the two nearest locations ")
-console.log(closestArr);
-console.log(idArr);
-*/
-/*
-
-fetch("https://evening-thicket-30478.herokuapp.com/soupKitchensSpecific/kitchId="+idArr[0], {
-  headers: {
-    'Content-Type': 'application/json',
-        'Accept': 'application/json'
-  }
-
-}).then((response) => response.json())
-  
-     .then(data => {
-        console.log("hello - we are getting the first location");
-        console.log(data); //should be the one specific item
-        this.setState({
-             closest[0].id : data._id,
-            closest[0].locName: data.FIELD1,
-            closest[0].address : data.FIELD4,
-            closest[0].locLat : data.FIELD11,
-            closest[0].locLong: data.FIELD12
-        })
-      })
-
-    .catch(error =>{
+      .catch(error =>{
       console.error(error)
-      })
+      }) 
 
-fetch("https://evening-thicket-30478.herokuapp.com/soupKitchensSpecific/kitchId="+idArr[1], {
-  headers: {
-    'Content-Type': 'application/json',
-        'Accept': 'application/json'
-  }
-
-}).then((response) => response.json())
-  
-     .then(data => {
-        console.log("hello - we are getting the first location");
-        console.log(data); //should be the one specific iteam
-        this.setState({
-             closest[1].id : data._id,
-            closest[1].locName: data.FIELD1,
-            closest[1].address : data.FIELD4,
-            closest[1].locLat : data.FIELD11,
-            closest[1].locLong: data.FIELD12
-        })
-      })
-      
-    .catch(error =>{
-      console.error(error)
-      })
-
-console.log(this.state.closest)
-  */  
    this.setState({ //have data is important to be true to get print to table to print
     haveData: true
    })
-  }
+  
+
+  } // close the access data
 
 
 //nameList index number might change based on returned data
